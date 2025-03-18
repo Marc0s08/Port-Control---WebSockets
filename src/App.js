d2cff1e8970133a4e08725858930fab3e6c98ce2
Marcos Eduardo <marcoseduc2019@gmail.com>
2025-03-18 13:57:33 -0300

    Alterando layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-
 const App = () => {
     const [socket, setSocket] = useState(null);
 
@@ -23,19 +22,12 @@ const App = () => {
     };
 
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1>Controle de porta</h1>
-            <button onClick={() => enviarComando('ligar')} style={botaoEstilo}>Abrir Porta</button>
-            <button onClick={() => enviarComando('desligar')} style={botaoEstilo}>Fechar Porta</button>
+        <div className="container">
+            <h1>Controle de Porta</h1>
+            <button onClick={() => enviarComando('ligar')} className="botao">Abrir Porta</button>
+            <button onClick={() => enviarComando('desligar')} className="botao">Fechar Porta</button>
         </div>
     );
-};
-
-const botaoEstilo = {
-    margin: '10px',
-    padding: '10px 20px',
-    fontSize: '18px',
-    cursor: 'pointer'
 };
 
 export default App;
