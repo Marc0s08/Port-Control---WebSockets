0c407b3e7175256cada0a700a44c9fdb46d620c0
Marcos Eduardo <marcoseduc2019@gmail.com>
2025-05-13 14:47:27 -0300

    aprimorando movimento dos micro servos

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
     const [socket, setSocket] = useState(null);
 
     useEffect(() => {
-        const newSocket = new WebSocket('ws://192.168.0.113/ws');
+        const newSocket = new WebSocket('ws://192.168.0.112/ws');
         newSocket.onopen = () => console.log('Conectado ao WebSocket');
         newSocket.onerror = (error) => console.error('Erro na conexão:', error);
         setSocket(newSocket);
@@ -16,16 +16,32 @@ const App = () => {
     const enviarComando = (comando) => {
         if (socket && socket.readyState === WebSocket.OPEN) {
             socket.send(comando);
-        } else {
-            console.warn('WebSocket não está conectado.');
         }
+    };
+
+    const iniciarEnvioContínuo = (comando) => {
+        enviarComando(comando); // envia logo no início
+        const interval = setInterval(() => enviarComando(comando), 100);
+
+        const parar = () => {
+            clearInterval(interval);
+            document.removeEventListener('mouseup', parar);
+            document.removeEventListener('touchend', parar);
+        };
+
+        document.addEventListener('mouseup', parar);
+        document.addEventListener('touchend', parar);
     };
 
     return (
         <div className="container">
-            <h1>Controle de Porta</h1>
-            <button onClick={() => enviarComando('ligar')} className="botao">Abrir Porta</button>
-            <button onClick={() => enviarComando('desligar')} className="botao">Fechar Porta</button>
+            <h1>Controle de posição servo motor</h1>
+            <button onMouseDown={() => iniciarEnvioContínuo('me')} className="botaovermelho">Mover servo Base esquerda</button>
+            <button onMouseDown={() => iniciarEnvioContínuo('md')} className="botaovermelho">Mover servo Base direita</button>
+            <button onMouseDown={() => iniciarEnvioContínuo('if')} className="botaoamarelo">Inclinar para frente</button>
+            <button onMouseDown={() => iniciarEnvioContínuo('it')} className="botaoamarelo">Inclinar para trás</button>
+            <button onMouseDown={() => iniciarEnvioContínuo('ag')} className="botaoverde">Abrir garra</button>
+            <button onMouseDown={() => iniciarEnvioContínuo('fg')} className="botaoverde">Fechar garra</button>
         </div>
     );
 };
